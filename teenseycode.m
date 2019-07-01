0146aa58e67021ae92220fd83883e9ff64a13f6f
YoussufSaleh <youssuf.saleh@ndcn.ox.ac.uk>
2019-07-01 10:52:24 +0100

    code

---
+++ teenseycode.m
@@ -0,0 +1,32 @@
+% Opens a serial port session
+try
+% fclose closes a file whose ID is typically contained within the brackets.
+% instrfind finds communication interface bobjects with specified property
+% values. 
+ 
+    fclose(instrfind) ;
+    
+end
+s = serial('/dev/tty.usbmodem40886101'); % Here you need to make sure you know the name of the serial port
+try
+    fopen(s);
+catch ME % in case it didn't work the first time
+    if (strcmp(ME.identifier,'MATLAB:serial:fopen:opfailed'))
+        fclose(instrfind);
+        fopen(s);
+    end
+ 
+end
+% Communicates with the serial port (send command, flush, read output)
+%
+ fprintf(s,'13pw');
+ fprintf(s,'10{1w1m0w0}')
+ flushinput(s)
+ flushoutput(s)
+ % This is the way you communicate with the device. In my case I want to send the '0e1s' command to the arduino.
+flushinput(s); % This is in case you want to flush the buffer
+%sscanf(bs(2),'%d'); % This is to query outputs from the device
+ 
+% Close the serial port session
+ 
+fclose(s);
