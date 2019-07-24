810d7597d81fcd8a36f4c3d70beec2a8979594c1
John Grogan <john.grogan@ndcn.ox.ac.uk>
2019-07-24 11:06:06 +0100

    made flushinputs only happen if using serial port, i.e. not if printing to console for testing purposes

--- TeenseyTrigger.m
+++ TeenseyTrigger.m
@@ -14,9 +14,11 @@ pinNumber = 2;% pin number that is connected in the teensey board
 % code is in value pinNumber, po (pin, outputmode), 1w (write 1),
 % trigValue,m (wait for trigValue duration), 0w (write 0)
 % so it sends a pulse, waits a duration, turns it off
-fprintf(portHandle, '%dpo1w%dm0w', pinNumber, trigValue);
+fprintf(portHandle, '%dpo1w%dm0w', pinNumber,trigValue);
 
-flushinput(portHandle); % empty input buffer
-flushoutput(portHandle); % empty output buffer
+if isa(portHandle, 'serial') % won't do this if just printing to console
+    flushinput(portHandle); % empty input buffer
+    flushoutput(portHandle); % empty output buffer
+end
 
 end
